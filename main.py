e6d09d7bd811379011ff915a3c53c9836eb061d0
RayJi <kaikai0428@gmail.com>
2020-04-06 23:35:50 +0800

    調整結構

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ line_header = {
     "Content-Type": "application/x-www-form-urlencoded"
 }
 
+# -------------------------------------------------------------------------------------------
 # 發送Line訊息
 
 
@@ -23,6 +24,7 @@ def NotifyLineMessage(msg):
         line_notify_url, headers=line_header, params=payload)
     return req_line.status_code
 
+# -------------------------------------------------------------------------------------------
 # 發送Line圖片
 
 
@@ -34,36 +36,10 @@ def NotifyLineImage(imageURL):
         line_notify_url, headers=line_header, params=payload)
     return req_line.status_code
 
-# -------------------------------------------------------------------------------------------
-# 搜尋頁面
-
-
-def searchPage(tag):
-    title = tag.text.strip()
-    page_url = base_url + tag.attrs['href']
-    page_req = requests.get(page_url, cookies={'over18': '1'})
-    page_soup = BeautifulSoup(page_req.text, 'html.parser')
-    pic_tag = page_soup.select('a[href]')
-    notify_msg = False
-    for i in range(len(pic_tag)):
-        file_name = os.path.basename(pic_tag[i].text)
-        # 確定有圖片才建立資料夾
-        if '.jpg' in file_name or '.png' in file_name:
-            if notify_msg == False:
-                notify_msg = True
-                print('開始下載......' + title)
-                NotifyLineMessage(title + "\n" + page_url)
-            NotifyLineImage(pic_tag[i].text)
-            #os.makedirs(title, exist_ok=True)
-            #img = requests.get(pic_tag[i].text)
-            #save = open(os.path.join(title, file_name), 'wb')
-            # for chunk in img.iter_content(100000):
-            #    save.write(chunk)
-            # save.close()
-
 
 # -------------------------------------------------------------------------------------------
-url_log_file = 'D:\\git\\py\\python-ptt\\log.txt'
+# 讀取log，用來判斷哪些新聞處理過
+url_log_file = 'D:\\git\\py\\python-ptt-beauty\\log.txt'
 log_file = codecs.open(
     url_log_file, 'r', encoding='utf-8')
 ptt_log = log_file.read().split('|')
@@ -71,20 +47,58 @@ log_file.close()
 
 base_url = 'https://www.ptt.cc'
 req_session = requests.session()
+# 讀取表特版
 req = requests.get(base_url + '/bbs/Beauty/index.html',
                    cookies={'over18': '1'})
 soup = BeautifulSoup(req.text, 'html.parser')
-tag = soup.select('.r-ent a')
 
-for i in range(len(tag)):
-    # 檢查標題是否已下載過
-    title = tag[i].text.strip()
+# 搜尋新聞格式的tag
+news_tag = soup.select('.r-ent a')
+
+for i in range(len(news_tag)):
+    # 標題
+    title = news_tag[i].text.strip()
+
+    # 略過不要的標題
     if '[公告]' in title or '[帥哥]' in title:
         continue
-    if title not in ptt_log:
-        ptt_log.append(title)
-        searchPage(tag[i])
 
+    # 略過已處理標題
+    if title in ptt_log:
+        continue
+
+    # 紀錄標題
+    ptt_log.append(title)
+
+    page_url = base_url + news_tag[i].attrs['href']  # 頁面連結
+    page_req = requests.get(page_url, cookies={'over18': '1'})  # 標記我已滿18
+    page_soup = BeautifulSoup(page_req.text, 'html.parser')
+    pic_tag = page_soup.select('a[href]')  # 搜尋所有圖片超連結
+    notify_title = False
+    for i in range(len(pic_tag)):
+        file_name = os.path.basename(pic_tag[i].text)
+        # 確定連結是圖片才處理
+        if '.jpg' in file_name or '.png' in file_name:
+            # 標題只發送一次
+            if notify_title == False:
+                notify_title = True
+                print('開始下載......' + title)
+
+                # 發送line訊息
+                NotifyLineMessage(title + "\n" + page_url)
+
+            # 發送line圖片
+            NotifyLineImage(pic_tag[i].text)
+
+            # 儲存本地圖片
+            #os.makedirs(title, exist_ok=True)
+            #img = requests.get(pic_tag[i].text)
+            #save = open(os.path.join(title, file_name), 'wb')
+            # for chunk in img.iter_content(100000):
+            #    save.write(chunk)
+            # save.close()
+
+# 儲存標題紀錄
 log_file = codecs.open(url_log_file, 'w', encoding='utf-8')
 log_file.truncate()  # 清空
 log_file.write("|".join(ptt_log))
